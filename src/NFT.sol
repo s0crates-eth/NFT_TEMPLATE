7c337ce166df607c140931e83b4324d0d062dca1
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-04-07 15:15:41 +0700

    Update NFT.sol

--- src/NFT.sol
+++ src/NFT.sol
@@ -10,7 +10,8 @@ contract NFT is ERC721, Ownable, DefaultOperatorFilterer {
     using Strings for uint256;
 
     address public erc20contract = 0xf0f9D895aCa5c8678f706FB8216fa22957685A13; // RVLT polygon token
-    uint256 public mintPrice = 186900000000000000000000000; // 186900000 ERC20 token
+    uint256 public erc20Price = 186900000000000000000000000; // 186900000 ERC20 token
+    uint256 public gasPrice = 1000000000000000; // 0.001 gas token
     uint256 public constant maxTokens = 3000;
     uint256 private constant tokensReserved = 20;
     uint256 public constant maxMintAmount = 10;
@@ -38,14 +39,16 @@ contract NFT is ERC721, Ownable, DefaultOperatorFilterer {
 
     // Public Functions
     function mint(uint256 _numTokens) external {
+    function mint(uint256 _numTokens) external payable {
         require(isSaleActive, "The sale is paused.");
         require(_numTokens <= maxMintAmount, "You cannot mint that many in one transaction.");
         require(mintedPerWallet[msg.sender] + _numTokens <= maxMintAmount, "You cannot mint that many total.");
         uint256 curTotalSupply = totalSupply;
         require(curTotalSupply + _numTokens <= maxTokens, "Exceeds total supply.");
-
-        // all users must APPROVE staking contract to use erc20 before v-this-v can work
-        bool success = IERC20(erc20contract).transferFrom(msg.sender, address(this), _amount);
+        require(_numTokens * gasPrice <= msg.value, "Insufficient funds.");
+        uint256 erc20Cost = erc20Price * _numTokens;
+        // users must APPROVE staking contract to use their erc20 before v-this-v can work
+        bool success = IERC20(erc20contract).transferFrom(msg.sender, address(this), erc20Cost);
         require(success == true, "transfer failed!");
 
         for(uint256 i = 1; i <= _numTokens; ++i) {
@@ -60,6 +63,14 @@ contract NFT is ERC721, Ownable, DefaultOperatorFilterer {
     // Owner-only functions
     function flipSaleState() external onlyOwner {
         isSaleActive = !isSaleActive;
+    }
+
+    function setGasPrice(uint256 _price) external onlyOwner {
+        gasPrice = _price;
+    }
+
+    function setERC20Price(uint256 _price) external onlyOwner {
+        erc20Price = _price;
     }
 
     function setBaseUri(string memory _baseUri) external onlyOwner {
@@ -79,15 +90,27 @@ contract NFT is ERC721, Ownable, DefaultOperatorFilterer {
         return baseUri;
     }
 
-    function withdrawAll() external payable onlyOwner {
-        uint256 balance = address(this).balance;
-        uint256 bonTreasury = balance * 70 / 100;
-        uint256 bonStakers = balance * 20 / 100;
-        uint256 bonDevs = balance * 10 / 100;
-        ( bool transferOne, ) = payable(0xd02b97b0B3439bf032a237f712a5fa5B161D89d3).call{value: bonTreasury}("");
-        ( bool transferTwo, ) = payable(0xad87F2c6934e6C777D95aF2204653B2082c453de).call{value: bonStakers}("");
-        ( bool transferThree, ) = payable(0xb1a23cD1dcB4F07C9d766f2776CAa81d33fa0Ede).call{value: bonDevs}("");
-        require(transferOne && transferTwo && transferThree, "Transfer failed.");
+    function withdrawAll(address address_70, address address_20, address address_10) external payable onlyOwner {
+        uint256 erc20Balance = IERC20(erc20contract).balanceOf(address(this));
+        uint256 gasBalance = address(this).balance;
+        if(erc20Balance > 0){
+            uint256 seventy_percent = erc20Balance * 70 / 100;
+            uint256 twenty_percent = erc20Balance * 20 / 100;
+            uint256 ten_percent = erc20Balance * 10 / 100;
+            bool transferOne = IERC20(erc20contract).transfer(address_70, seventy_percent);
+            bool transferTwo = IERC20(erc20contract).transfer(address_20, twenty_percent);
+            bool transferThree = IERC20(erc20contract).transfer(address_10, ten_percent);
+            require(transferOne && transferTwo && transferThree, "transfer failed!");
+        }
+        if(gasBalance > 0){
+            uint256 seventy_percent = gasBalance * 70 / 100;
+            uint256 twenty_percent = gasBalance * 20 / 100;
+            uint256 ten_percent = gasBalance * 10 / 100;
+            ( bool transferOne, ) = payable(address_70).call{value: seventy_percent}("");
+            ( bool transferTwo, ) = payable(address_20).call{value: twenty_percent}("");
+            ( bool transferThree, ) = payable(address_10).call{value: ten_percent}("");
+            require(transferOne && transferTwo && transferThree, "Transfer failed.");
+        }
     }
  
     // OpenSea Enforcer functions
