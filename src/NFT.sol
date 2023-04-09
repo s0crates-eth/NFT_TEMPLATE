dd0b0719d65c0c62f0abf0c07bc8603b1daa829a
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-04-09 16:20:34 +0700

    Update NFT.sol

--- src/NFT.sol
+++ src/NFT.sol
@@ -2,6 +2,11 @@
 
 pragma solidity ^0.8.9;
 
+/* NOTES
+>> add a second option to pay with BON
+- two buttons sidebyside (PAY WITH BON) for approvals
+- then one button below to buy with mint amount
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
