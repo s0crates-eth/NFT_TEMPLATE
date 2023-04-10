1c8f072938f638bc91384c5bf89f71c636fb28e3
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-04-10 16:29:55 +0700

    updates to the test file

--- test/NFT.t.sol
+++ test/NFT.t.sol
@@ -17,10 +17,15 @@ import "../src/NFT.sol";
 
 contract contractTest is Test {
     NFT public contractTested;
+    address public RVLT = 0xf0f9D895aCa5c8678f706FB8216fa22957685A13;
 
     function setUp() public{
+
         address user = address(69);
         vm.startPrank(user);
+
+        vm.deal(user, 1_000_000 ether);
+        deal(RVLT, user, 1_000_000_000 ether);
         
         address[] memory testAddresses = new address[](3);
         testAddresses[0] = address(70);
@@ -35,9 +40,10 @@ contract contractTest is Test {
     }
 
     function test_mintNormally() public{
+        IERC20(RVLT).approve(address(69), 186900000000000000000000000 * 2);
         contractTested.flipSaleState();
-        contractTested.mint(2);
-        assertEq(contractTested.totalSupply(), 25);
+        //contractTested.mint{value: 1 ether}(2);
+        //assertEq(contractTested.totalSupply(), 25);
     }
 
     function testFail_mintButSaleNotActive() public{
