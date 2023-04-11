ebc261ab0596ec5b9075e10fb7b76b414336f7ab
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-04-11 12:27:00 +0700

    testing file updates

--- blerp.md
+++ blerp.md
@@ -4,4 +4,4 @@
 # NOTES:
 - forge test --match-path test/NFT.t.sol -vv
 - https://nftstorage.link/ipfs/bafybeihscowmgmn6evsax36ys57hreo744vj5lgie3m6yjrb2u2vjufpiq
-- forge test --fork-url https://polygon-mainnet.g.alchemy.com/v2/elpiyNU3HOchYaeMMpCteXolAFqJYTEi -vv
+- forge test --fork-url https://polygon-mainnet.g.alchemy.com/v2/elpiyNU3HOchYaeMMpCteXolAFqJYTEi -vvv
--- test/NFT.t.sol
+++ test/NFT.t.sol
@@ -3,12 +3,8 @@ pragma solidity ^0.8.17;
 
 /*
 NOTES:
-- to run this contract, 'forge test --match-path test/FILEnAME.t.sol'
-- setUp is called again (fresh vars) after each test function
-- all tests need to be preceded by 'testXXX'
-- all expected to fail tests need to be preceded by 'testFailXXX'
-- to get more details; 'forge test --match-path test/NFT.t.sol -vvvvv'
-- for gas details; 'forge test --match-path test/NFT.t.sol --gas-report'
+- for more testing ideas, see: https://github.com/s0crates-eth/foundry_sandbox/blob/main/test/NFT.t.sol
+
 */
 
 
@@ -20,18 +16,23 @@ contract contractTest is Test {
     address public RVLT = 0xf0f9D895aCa5c8678f706FB8216fa22957685A13;
 
     function setUp() public{
-
+        // wallets setup
         address user = address(69);
         vm.startPrank(user);
-
-        vm.deal(user, 1_000_000 ether);
-        deal(RVLT, user, 1_000_000_000 ether);
-        
         address[] memory testAddresses = new address[](3);
-        testAddresses[0] = address(70);
-        testAddresses[1] = address(71);
-        testAddresses[2] = address(72);
+            testAddresses[0] = address(70);
+            testAddresses[1] = address(71);
+            testAddresses[2] = address(72);
 
+        // token setup
+        vm.deal(user, 1_000_000 ether);
+        deal(RVLT, user, 1_000_000_000_000 ether);
+        bool success = IERC20(RVLT).approve(
+            0x8fA079a96cE08F6E8A53c1C00566c434b248BFC4, 
+            115792089237316195423570985008687907853269984665640564039457584007913129639935);
+        require(success, "Approve failed");
+        
+        // contract setup
         contractTested = new NFT(testAddresses);
     }
 
@@ -39,20 +40,56 @@ contract contractTest is Test {
         assertEq(contractTested.totalSupply(), 23);
     }
 
-    function test_mintNormally() public{
-        IERC20(RVLT).approve(address(69), 186900000000000000000000000 * 2);
+    function test_nftTokenTransferAndMinting() public{
         contractTested.flipSaleState();
-        //contractTested.mint{value: 1 ether}(2);
-        //assertEq(contractTested.totalSupply(), 25);
+        uint beforeErcBal = IERC20(RVLT).balanceOf(address(69));
+        contractTested.mint{value: 1 ether}(2);
+        uint afterErcBal = IERC20(RVLT).balanceOf(address(69));
+        assertFalse(beforeErcBal == afterErcBal);
+        assertEq(contractTested.totalSupply(), 25);
+    }
+
+    function testFail_mintingGasNotEnnough() public{
+        contractTested.flipSaleState();
+        contractTested.mint{value: 1}(2);
     }
 
     function testFail_mintButSaleNotActive() public{
-        contractTested.mint(2);
+        contractTested.mint{value: 1 ether}(2);
     }
     
     function testFail_mintSurpassMax() public{
         contractTested.flipSaleState();
         contractTested.mint(11);
     }
+
+    function test_withdrawAll() public{
+        contractTested.flipSaleState();
+        contractTested.mint{value: 1 ether}(2);
+        uint b4Wallet70erc = IERC20(RVLT).balanceOf(address(70));
+        uint b4Wallet71erc = IERC20(RVLT).balanceOf(address(71));
+        uint b4Wallet72erc = IERC20(RVLT).balanceOf(address(72));
+        uint b4Wallet70gas = address(70).balance;
+        uint b4Wallet71gas = address(71).balance;
+        uint b4Wallet72gas = address(72).balance;
+        contractTested.withdrawAll(address(70), address(71), address(72));
+        uint AftWallet70erc = IERC20(RVLT).balanceOf(address(70));
+        uint AftWallet71erc = IERC20(RVLT).balanceOf(address(71));
+        uint AftWallet72erc = IERC20(RVLT).balanceOf(address(72));
+        uint AftWallet70gas = address(70).balance;
+        uint AftWallet71gas = address(71).balance;
+        uint AftWallet72gas = address(72).balance;
+        assertTrue(b4Wallet70erc == 0);
+        assertFalse(AftWallet70erc == 0);
+        assertTrue(b4Wallet70gas == 0);
+        assertFalse(AftWallet70gas == 0);
+        assertFalse(b4Wallet70erc == AftWallet70erc);
+        assertFalse(b4Wallet70gas == AftWallet70gas);
+        assertFalse(b4Wallet71erc == AftWallet71erc);
+        assertFalse(b4Wallet71gas == AftWallet71gas);
+        assertFalse(b4Wallet72erc == AftWallet72erc);
+        assertFalse(b4Wallet72gas == AftWallet72gas);
+    }
+
 
 }
